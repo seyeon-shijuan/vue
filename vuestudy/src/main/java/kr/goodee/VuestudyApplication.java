b9eb923f635a919676bc49d0d65d38b95ad59e7c
seyeon-shijuan <66824510+seyeon-shijuan@users.noreply.github.com>
2020-09-09 18:42:14 +0900

    200909

---
+++ vuestudy/src/main/java/kr/goodee/VuestudyApplication.java
@@ -0,0 +1,13 @@
+package kr.goodee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuestudyApplication.class, args);
+	}
+
+}
---
+++ vuestudy/src/test/java/kr/goodee/VuestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.goodee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
